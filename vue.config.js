688ca69147ff1bce99381320971ff15d07b32c95
xiaolaodi-star <2249205175@qq.com>
2022-04-28 18:38:20 +0800

    learn vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports={
+  pages:{
+    index:{
+      entry:"./src/module/index/main.js",
+      template:"./src/module/index/index.html",
+      filename:"index.html",
+      title:"首页"
+    }
+  }
+}
